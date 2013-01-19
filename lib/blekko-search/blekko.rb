4a1d3288a2bb091d4a36181231213e21c6c872d8
Sean Devine <barelyknown@icloud.com>
2013-01-19 15:43:38 -0600

    add user agent

--- lib/blekko-search/blekko.rb
+++ lib/blekko-search/blekko.rb
@@ -3,16 +3,16 @@ class Blekko
   DEFAULT_MAX_FREQUENCY_PER_SECOND = 1
   SECURE_PROTOCOL = "https://"
   NON_SECURE_PROTOCOL = "http://"
-  
+
   class << self
     attr_accessor :last_request_at
     def last_request_at
       @last_request_at ||= {}
     end
   end
-      
+
   attr_accessor :protocol, :api_key, :max_frequency_per_second, :username, :password, :login_cookie, :headers
-      
+
   def initialize(args={})
     @api_key = args[:api_key]
     @protocol = args[:secure] ? SECURE_PROTOCOL : NON_SECURE_PROTOCOL
@@ -21,33 +21,33 @@ class Blekko
     @max_frequency_per_second = args[:max_frequency_per_second] || DEFAULT_MAX_FREQUENCY_PER_SECOND
     login if @api_key && @username && @password
   end
-  
+
   def host
     HOST
   end
-  
+
   def search(query, args={})
     Blekko::Search.new(self, query, args).search
   end
-  
+
   def slashtag(name, args={})
     Blekko::Slashtag.new(self, name, args)
   end
-  
+
   def request(url)
     sleep(seconds_until_next_request)
     self.last_request_at = Time.now
     open(url, headers)
   end
-  
+
   def login_uri
     URI("#{SECURE_PROTOCOL}#{HOST}/login?u=#{CGI.escape(username)}&p=#{CGI.escape(password)}&auth=#{api_key}")
   end
-  
+
   def headers
-    @headers ||= {}
+    @headers ||= { "User-Agent" => "blekko-search-#{BlekkoSearch::VERSION}" }
   end
-  
+
   def login
     raise ArgumentError, "Username and password are required" unless username && password
     Net::HTTP.start(login_uri.host, login_uri.port, use_ssl: true) do |http|
@@ -57,25 +57,25 @@ class Blekko
       headers["User-Agent"] = "blekko-search-#{BlekkoSearch::VERSION}"
     end
   end
-  
+
   def delay_between_requests
     1 / max_frequency_per_second.to_f
   end
-  
+
   def last_request_at
     self.class.last_request_at[api_key]
   end
-  
+
   def last_request_at=(value)
     self.class.last_request_at[api_key] = value
   end
-  
+
   def earliest_next_request
     last_request_at ? last_request_at + delay_between_requests : Time.now
   end
-  
+
   def seconds_until_next_request
     [earliest_next_request - Time.now, 0].max
   end
-    
+
 end
